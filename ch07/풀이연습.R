008c2da6958549c7fb2baecfc9c3510ca0a11f21
Jiwon Moon <hannah.jwmoon@gmail.com>
2021-04-29 17:01:46 +0900

    Chapter 7

---
+++ ch07/풀이연습.R
@@ -0,0 +1,18 @@
+library(dplyr)
+galton <-read.table('data/Galton.txt', header = T)
+galton
+head(galton)
+
+df <- galton %>% 
+   filter(Gender== 'M') %>% 
+   select(Father, Height)
+head(df)
+
+df_m <- df %>% 
+   mutate(father= Father*2.54, son= Height*2.54)
+head(df_m)
+
+plot(df)
+model <- lm(son ~ father, data= df_m)
+coef(model)
+abline(model, col= 'red', lwd =3)
