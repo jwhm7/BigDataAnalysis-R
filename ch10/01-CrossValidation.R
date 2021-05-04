4d7ff75f7b0547e1248c473e56a6c90d53827e0f
Jiwon Moon <hannah.jwmoon@gmail.com>
2021-05-04 11:01:03 +0900

    Chapter 10

---
+++ ch10/01-CrossValidation.R
@@ -0,0 +1,35 @@
+# 교차 검증(Cross Validation)
+library(caret)
+
+# iris 데이터를 섞는다.
+set.seed(2021)
+data <- iris[sample(nrow(iris)),]
+head(data)
+tail(data)
+
+# K-Fold CV, K=5
+k <- 5
+q <- nrow(data) / k
+l <- 1:nrow(data)
+acc <- 0
+
+for(i in 1:k){
+  test_list <- ((i-1)*q+1) : (i*q)
+  data_test <- data[test_list, ]
+  data_train <- data[-test_list, ]
+  rf <- train(Species~., data_train, method= 'rf')
+  pred <- predict(rf, data_test)
+  t <- table(pred, data_test$Species)
+  acc <- acc + (t[1,1] + t[2,2] + t[3,3]) / nrow(data_test)
+}
+
+average_accuracy <- acc / k
+average_accuracy
+
+##########################################################
+# caret 라이브러리를 이용한 코드
+##########################################################
+control <- trainControl(method='cv', number= 5)
+new_rf <- train(Species~., data= iris, method='rf',
+                metric= 'Accuracy', trControl= control)
+confusionMatrix(new_rf)
