0e61b4b14398e384cf24eaeeafba2c61dce3e0c5
Jiwon Moon <hannah.jwmoon@gmail.com>
2021-05-10 14:12:17 +0900

    Crawling

--- crawling/01-한빛미디어.R
+++ crawling/01-한빛미디어.R
@@ -57,15 +57,82 @@ book_url
 book_html <- read_html(book_url)
 info_list <- html_node(book_html, 'ul.info_list')
 lis <- html_nodes(info_list, 'li')
-page_li <- lis[4]
-page <- html_node(page_li, 'span') %>% html_text()
+for (li in lis) {
+   item <- li %>% 
+      html_node('strong') %>% 
+      html_text()
+   if(substring(item, 1, 3)== '페이지') {
+      page <- li %>% 
+         html_node('span') %>% 
+         html_text()
+      len <- nchar(page)
+      page <- as.integer(substring(page, 1, len-2))
+      break
+   }
+}
 page
 # 내가 찾은 정수화 방법
 string <- page
 strtoi(string)
 as.integer(284)
-# 방법
-len <- nchar(page)
-len
-page <- as.integer(substring(page, 1, len-2))
-page
+
+pay_info <- html_node(book_html, '.payment_box.curr')
+ps <- html_nodes(pay_info, 'p')
+price <- ps[2] %>% 
+   html_node('.pbr') %>% 
+   html_node('strong') %>% 
+   html_text()
+price <- as.integer(gsub(',', '', price))
+price
+
+
+container <- html_node(html, '#container') 
+book_list <- html_node(container, '.new_book_list_wrap')
+sub_book_list <- html_node(book_list, '.sub_book_list_area')
+lis <- html_nodes(sub_book_list, 'li')
+title_vector <- c()
+writer_vector <- c()
+page_vector <- c()
+price_vector <- c()
+for (li in lis) {
+   info <- html_node(li, '.info')
+   title <- info %>% 
+      html_node('.book_tit') %>% 
+      html_text()
+   writer <- info %>%
+      html_node('.book_writer') %>% 
+      html_text()
+   href <- li %>% 
+      html_node('.info') %>% 
+      html_node('a') %>% 
+      html_attr('href')
+   book_url <- paste(base_url, href, sep= '/')
+   book_html <- read_html(book_url)
+   info_list <- html_node(book_html, 'ul.info_list')
+   book_lis <- html_nodes(info_list, 'li')
+   for (book_li in book_lis) {
+      item <- li %>% 
+         html_node('strong') %>% 
+         html_text()
+      if(substring(item, 1, 3)== '페이지') {
+         page <- li %>% 
+            html_node('span') %>% 
+            html_text()
+         len <- nchar(page)
+         page <- as.integer(substring(page, 1, len-2))
+         break
+      }
+   }
+   pay_info <- html_node(book_html, '.payment_box.curr')
+   ps <- html_nodes(pay_info, 'p')
+   price <- ps[2] %>% 
+      html_node('.pbr') %>% 
+      html_node('strong') %>% 
+      html_text()
+   price <- as.integer(gsub(',', '', price))
+   
+   title_vector <- c(title_vector, title)
+   writer_vector <- c(writer_vector, writer)
+   page_vector <- c(page_vector, page)
+   price_vector <- c(price_vector, price)
+}
